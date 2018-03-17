4e7fd2d9a2c4c0bf16dfa9a09f8c4ad7e9076658
Sayan Faraz <sayan96@hotmail.com>
2018-03-17 17:07:27 -0400

    using index for x axis instaed of time

--- scripts/analyze_data.py
+++ scripts/analyze_data.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import biosppy
 
+from sklearn.preprocessing import scale
 
 def bandpower(data, sample_rate, low_freq, high_freq):
     calculated_freqs, power_spectrum = biosppy.tools.\
@@ -44,14 +45,14 @@ def bandpass(data, sample_rate, low_freq, high_freq):
     return filtered_data
 
 
-def fill_bandpower(data, time, sample_rate):
+def fill_bandpower(data, time, sample_rate, low_freq, high_freq):
     mu_power_c3 = np.array([])
     c3_packet = np.array([])
     curr_gesture = gesture.item(0)
     for i in range(0, len(time)):
         # Calculate bandpower for packet, and reset packet
         if gesture.item(i) != curr_gesture or i == len(time) - 1:
-            power = bandpower(c3_packet, sample_rate, 7.0, 13.0)
+            power = bandpower(c3_packet, sample_rate, low_freq, high_freq)
             for sample in c3_packet:
                 mu_power_c3 = np.append(mu_power_c3, power)
             curr_gesture = gesture.item(i)
@@ -84,31 +85,35 @@ if __name__ == '__main__':
 
     sample_rate = 256.0
 
-    with open("../eeg/motor-imagery/karl-c3-c4-hand-ext-flex-2018-03-07-round1.csv",
+    with open("../eeg/motor-imagery/2018-03-16/karl-exp1-trial1.csv",
+    # with open("../eeg/motor-imagery/karl-c3-c4-hand-ext-flex-2018-03-10-round12.csv",
               "r") as \
             data_file:
         data_reader = csv.reader(data_file, delimiter=",", quotechar="|")
-        id = 0
-        factor = 1000000.0
+        i = 0
+        factor = 1
         for row in data_reader:
             # if id < max_id:
             time = np.append(time, [float(row[0])])
-            index = np.append(index, [float(row[1])])
-            c3 = np.append(c3, [float(row[2]) / factor])
-            c4 = np.append(c4, [float(row[3]) / factor])
-            gesture = np.append(gesture, [float(row[11])])
-            id += 1
+            # index = np.append(index, [float(row[1]) + i * 256.0])
+            index = np.append(index, [i])
+            c3 = np.append(c3, [float(row[4])])
+            c4 = np.append(c4, [float(row[5])])
+            gesture = np.append(gesture, [float(row[11]) * factor])
+            i += 1
 
     analyze_data = c3
-    analyze_data = np.subtract(analyze_data, np.full((len(analyze_data)), analyze_data.item(0)))
+    # analyze_data = np.subtract(analyze_data, np.full((len(analyze_data)), analyze_data.item(0)))
+    analyze_data = np.subtract(analyze_data, np.full((len(analyze_data)), np.mean(c3)))
 
-    cleaned_data = bandpass(highpass(analyze_data, sample_rate, 2), sample_rate, 2.0, 50.0)
+    # cleaned_data = bandpass(highpass(analyze_data, sample_rate, 2), sample_rate, 2.0, 50.0)
+    cleaned_data = bandpass(analyze_data, sample_rate, 2.0, 50.0)
 
-    # mu_c3 = bandpass(clean_c3, 256.0, 7.0, 13.0)
+    mu = bandpass(cleaned_data, 256.0, 7.0, 13.0)
 
-    mu_power = fill_bandpower(cleaned_data, time, sample_rate)
+    mu_power = scale(np.reshape(fill_bandpower(mu, time, sample_rate, 7.0, 13.0), (-1, 1))).ravel()
 
-    # arr = np.array(time, mu_power_c3)
+    # arr = np.array(time, mu_power)
 
     # print(len(mu_power_c3))
     # print(len(time))
